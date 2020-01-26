007b5b91525aeaa38c37d1244dca0ccb0ad7eda4
Jonatas <modesto.jonatas18@gmail.com>
2020-01-26 17:22:07 -0300

    Funcoes 'CopyTo' e 'ReadAllLines'

---
+++ Trabalhando com Arquivos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Trabalhando_com_Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\File\File1.txt";
+            string targetPath = @"c:\File\File2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
